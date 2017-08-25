52f84db1fa36234b6131cd18a0afdc646f18208d
Ryan <ryan.hartlage@geappliances.com>
2017-08-25 13:40:21 -0400

    This might work

--- http_server/init.lua
+++ http_server/init.lua
@@ -1,7 +1,7 @@
 local function send_header(status, connection, callback)
   local message = {
     [200] = 'OK',
-    [404] = 'Not found'
+    [404] = 'FILE NOT FOUND'
   }
 
   local header =
@@ -28,28 +28,62 @@ local function send_file(filename, connection, callback)
   send_next_chunk()
 end
 
+local function parse_request(request)
+  local args = {}
+  local method, resource = request:match('(%w*) ([^%?%s]*)%??')
+  local arg_string = request:match('?([^%s]*)') or ''
+
+  for name, value in arg_string:gmatch('(%w*)=(%w*)') do
+    args[name] = value
+  end
+
+  return {
+    method = method,
+    resource = resource,
+    args = args
+  }
+end
+
 net.createServer(net.TCP):listen(80, function(connection)
   connection:on('receive', function(response, request)
     print(request)
 
-    send_header(200, response, function()
-      send_file('index.html', response, function()
-        response:close()
+    request = parse_request(request)
+
+    if request.method == 'GET' then
+      if request.resource == '/' then
+        send_header(200, response, function()
+          send_file('index.html', response, function()
+            response:close()
+          end)
+        end)
+      else
+        send_header(404, response, function()
+          send_file('whoops.html', response, function()
+            response:close()
+          end)
+        end)
+      end
+    elseif request.method == 'POST' then
+      if request.resource == '/gpio' then
+        gpio.write(args.pin, args.state)
+
+        send_header(200, response, function()
+          response:close()
+        end)
+      else
+        send_header(404, response, function()
+          send_file('whoops.html', response, function()
+            response:close()
+          end)
+        end)
+      end
+    else
+      send_header(404, response, function()
+        send_file('whoops.html', response, function()
+          response:close()
+        end)
       end)
-    end)
+    end
   end)
 end)
-
-local pin = 1
-
-gpio.mode(pin, gpio.OUTPUT)
-
-tmr.create():alarm(500, tmr.ALARM_AUTO, coroutine.wrap(function()
-  while true do
-    gpio.write(pin, gpio.HIGH)
-    coroutine.yield()
-
-    gpio.write(pin, gpio.LOW)
-    coroutine.yield()
-  end
-end))
