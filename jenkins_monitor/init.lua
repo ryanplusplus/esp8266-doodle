ff094dc0ca3cfc4a6e7490e1a5d5e2ee50dcf006
Ryan Hartlage <ryanplusplus@gmail.com>
2018-03-19 20:24:17 -0400

    Almost kinda sorta getting data from Jenkins

--- jenkins_monitor/init.lua
+++ jenkins_monitor/init.lua
@@ -1,35 +1,54 @@
-local display = dofile('pcd8544.lc')
--- local display = dofile('ssd1406.lc')
+coroutine.resume(coroutine.create(function()
+  local display = dofile('pcd8544.lc')
+  -- local display = dofile('ssd1406.lc')
 
-display:setFont(u8g.font_chikita)
-display:setFontRefHeightExtendedText()
-display:setDefaultForegroundColor()
-display:setFontPosTop()
+  display:setFont(u8g.font_chikita)
+  display:setFontRefHeightExtendedText()
+  display:setDefaultForegroundColor()
+  display:setFontPosTop()
 
-local function draw(f, on_complete)
-  display:firstPage()
+  local function draw(f)
+    local co = coroutine.running()
+    display:firstPage()
 
-  local function draw_page()
-    f()
+    while true do
+      f()
 
-    if display:nextPage() then
-      node.task.post(draw_page)
-    elseif on_complete then
-      on_complete()
+      if display:nextPage() then
+        node.task.post(function() coroutine.resume(co) end)
+        coroutine.yield()
+      else
+        break
+      end
     end
   end
 
-  draw_page()
-end
+  draw(function()
+    display:drawStr(0, 0, 'Something')
+    display:drawStr(0, 25, 'Another something')
+  end)
 
-draw(function()
-  display:drawStr(0, 0, 'Something')
-  display:drawStr(0, 25, 'Another something')
-end)
+  local json_decoder = sjson.decoder({ metatable = {
+    checkpath = function(_, path)
+      print(unpack(path))
+    end
+  } })
 
-local socket = tls.createConnection()
-socket:on('receive', function(_, data) print(data) end)
-socket:on('connection', function()
-  socket:send('GET / HTTP/1.1\r\nHost: www.google.com\r\nAccept: */*\r\n\r\n')
-end)
-socket:connect(443, 'www.google.com')
+  local got_header
+  local socket = tls.createConnection()
+  socket:on('receive', function(_, data)
+    if not got_header then
+      local data = data:match('\r\n\r\n(.*)')
+      if data then
+        got_header = true
+        json_decoder:write(data)
+      end
+    else
+      json_decoder:write(data)
+    end
+  end)
+  socket:on('connection', function()
+    socket:send('GET /computer/api/json HTTP/1.1\r\nHost: ci.openquake.org\r\nAccept: */*\r\n\r\n')
+  end)
+  socket:connect(443, 'ci.openquake.org')
+end))
